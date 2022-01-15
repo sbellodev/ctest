93b451ae2114b3480288d979cdc3fb97d33e9e73
Santi B <sbellodev@gmail.com>
2022-01-15 19:13:17 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
+//Console.WriteLine("Hello, World!");
